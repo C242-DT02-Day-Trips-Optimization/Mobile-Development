3d7a0a9d42af65579c374c9a30c4fd1b42e576a4
jonathanbasuki <basukijonathan4@gmail.com>
2024-12-07 12:14:21 +0700

    added ui for new trips destination and modify using viewbinding

--- app/src/main/java/com/bizzagi/daytripoptimization/team2/ui/home/NewDestinationActivity.kt
+++ app/src/main/java/com/bizzagi/daytripoptimization/team2/ui/home/NewDestinationActivity.kt
@@ -2,15 +2,10 @@ package com.bizzagi.daytripoptimization.team2.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bizzagi.daytripoptimization.team2.R
 import com.bizzagi.daytripoptimization.team2.databinding.ActivityNewDestinationBinding
 import com.bizzagi.daytripoptimization.team2.databinding.ItemNewDestinationBinding
